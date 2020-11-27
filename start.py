48767cbff834c3edc8c39dbd21cc8fb226481e4c
Shweta <shwetagupta9411@gmail.com>
2020-11-27 19:33:26 +0000

    returning response in json

--- start.py
+++ start.py
@@ -2,18 +2,25 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 from airline_test import TestAirlineSentiment
+import json
+
+from pyramid.response import Response
+from pyramid.response import response_adapter
 
 """
 sample request
 http://localhost:6543?review=@AIRLINE $ % StAff I hate the services http://www.google.com
 
 """
+@response_adapter(dict, list)
 def airline_sentiment_prediction(request):
-    print("test")
     model_name = "airline_sentiment_model_20201123025610.sav"
     review = request.GET.get('review')
     res = TestAirlineSentiment(model_name, review).test()
-    return Response(res)
+    response = {
+    "review": res
+    }
+    return Response(json.dumps(response))
 
 
 if __name__ == '__main__':
